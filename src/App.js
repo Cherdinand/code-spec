3c0523490bb83fdcd23c4fef1cc87eae6d823b16
Cherdinand <736119088@qq.com>
2023-06-06 11:09:23 +0800

    lint

--- src/App.js
+++ src/App.js
@@ -2,46 +2,46 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const a = () => {
-    console.log('111:', 111);
-  };
+	const a = () => {
+		console.log('111:', 111);
+	};
 
-  const xinxin = {
-    age: 11,
-  };
+	const xinxin = {
+		age: 11,
+	};
 
-  const b = new Proxy(xinxin, {
-    get: (target) => {
-      return target;
-    },
-    set: function (target, propKey, receiver) {
-      target[propKey] = receiver;
-      return target;
-    },
-  });
+	const b = new Proxy(xinxin, {
+		get: (target) => {
+			return target;
+		},
+		set: function (target, propKey, receiver) {
+			target[propKey] = receiver;
+			return target;
+		},
+	});
 
-  const c = new Set([1, 2, 3]);
+	const c = new Set([1, 2, 3]);
 
-  console.log('b:', b, c);
+	console.log('b:', b, c);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={a}>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p onClick={a}>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
